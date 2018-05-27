8457920c582fc2e58c88c9a79fa0ff2c65222f32
harsh-gulati11 <harsh.gulati@outlook.com>
2018-05-27 18:53:47 +0530

    Practice commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { ListEmployeesComponent } from './employees/list-employees.component';
+import { CreateEmployeeComponent } from './employees/create-employee.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  { path: 'list', component: ListEmployeesComponent },
+  { path: 'create', component: CreateEmployeeComponent },
+  { path: '', redirectTo: '/list', pathMatch: 'full' }];
+@NgModule({
+  declarations: [
+    AppComponent,
+    ListEmployeesComponent,
+    CreateEmployeeComponent
+  ],
+  imports: [
+    // tslint:disable-next-line:whitespace
+    BrowserModule, 
+    RouterModule.forRoot(appRoutes)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/employees/list-employees.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+import { Employee } from '../Models/employee.model';
+
+@Component({
+  // selector: 'app-list-employees',
+  templateUrl: './list-employees.component.html',
+  styleUrls: ['./list-employees.component.css']
+})
+export class ListEmployeesComponent implements OnInit {
+
+  employees: Employee[] = [{
+    id: 1,
+    name: 'Harsh Gulati',
+    gender: 'Male',
+    email: 'harsh.gulati@outlook.com',
+    phoneNumber: 7898121301,
+    contactPreference: 'Komal Gulati',
+    dateOfBirth: new Date('05/11/1986'),
+    department: 'Home',
+    isActive: true,
+    photoPath: 'assets/images/harsh.jpg'
+  },
+  {
+    id: 2,
+    name: 'Komal Gulati',
+    gender: 'female',
+    email: 'komal.gulati@outlook.com',
+    phoneNumber: 7898121301,
+    contactPreference: 'Komal Gulati',
+    dateOfBirth: new Date('01/12/1987'),
+    department: 'Ministry',
+    isActive: true,
+    photoPath: 'assets/images/harsh.jpg'
+  },
+  {
+    id: 2,
+    name: 'Vansh Gulati',
+    gender: 'male',
+    email: 'vansh.gulati@outlook.com',
+    phoneNumber: 7898121301,
+    contactPreference: 'vansh Gulati',
+    dateOfBirth: new Date('05/08/2017'),
+    department: 'sweetheart',
+    isActive: false,
+    photoPath: 'assets/images/harsh.jpg'
+  }
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
